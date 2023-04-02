5fadfe71c04ab5a51d48aee1b55ce02e3371bf53
Nefar10 <pinnlab@gmail.com>
2023-04-02 20:50:39 +0300

    3

---
+++ app/src/main/java/com/example/sbook/AddNoteActivity.java
@@ -0,0 +1,50 @@
+package com.example.sbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddNoteActivity extends AppCompatActivity {
+    EditText etText;
+    int id = -1;
+    String text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            text = bundle.getString("txt");
+            id = bundle.getInt("id");
+            etText = (EditText) findViewById(R.id.et_text);
+            etText.setText(text);
+        }
+    }
+    public void accept (View ImageButton) {
+        text = etText.getText().toString();
+        if (text.length() > 0 && id > -1) {
+            Intent add_note = new Intent();
+            add_note.putExtra("result",text);
+            add_note.putExtra("id",id);
+            setResult(RESULT_OK,add_note);
+            finish();
+        }
+    }
+    public void cancel (View ImageButton) {
+        Intent add_note = new Intent();
+        add_note.putExtra("result",text);
+        add_note.putExtra("id",-1);
+        setResult(RESULT_OK,add_note);
+        finish();
+    }
+    public void delete (View ImageButton) {
+        Intent add_note = new Intent();
+        add_note.putExtra("result","");
+        add_note.putExtra("id",id);
+        setResult(RESULT_OK,add_note);
+        finish();
+    }
+}
--- app/src/main/java/com/example/sbook/MainActivity.java
+++ app/src/main/java/com/example/sbook/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.sbook;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.NestedScrollView;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
     List<String> myNotes = new ArrayList<String>();
     Boolean wasInit = false;
+    //Настройки
+    Boolean set_big_text = false;
+    Boolean set_hints = true;
+    int set_color = 0xE6E100;
+    int set_opacity = 0x88;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
         mySLayout.setOrientation(LinearLayout.VERTICAL);
         for (int i = 1; i <= myNotes.size(); i++) {
             TextView noteView = new TextView(this);
-            noteView.setText(myNotes.get(i-1));
             Log.i("myInfo",noteView.getText().toString());
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT, // ширина элемента
@@ -48,18 +55,25 @@ public class MainActivity extends AppCompatActivity {
             );
             params.setMargins(20,20,20,20);
             noteView.setLayoutParams(params);
+            noteView.setId(i+2000000);
+            noteView.setText(myNotes.get(i-1));
             noteView.setTextSize(20);
             noteView.setMinHeight(140);
             noteView.setTextColor(0xFF000000);
             noteView.setGravity(Gravity.CENTER_VERTICAL);
             noteView.setBackgroundResource(R.drawable.rectangle_2);
+            noteView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Toast.makeText(getApplicationContext(), noteView.getText(), Toast.LENGTH_SHORT).show();
+                    Intent add_note = new Intent(MainActivity.this, AddNoteActivity.class);
+                    add_note.putExtra("id", noteView.getId());
+                    add_note.putExtra("txt", noteView.getText());
+                    startActivityForResult(add_note, 1);
+                }
+            });
             mySLayout.addView(noteView);
         }
         mySView.addView(mySLayout);
     }
-    /*
-        <TextView
-
-        android:onClick="editNote"/>
-     */
 }

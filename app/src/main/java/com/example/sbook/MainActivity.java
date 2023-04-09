cc0b9ebf121cb89baf1eb05f45f0936cd9613c12
Nefar10 <pinnlab@gmail.com>
2023-04-09 23:28:00 +0300

    6

--- app/src/main/java/com/example/sbook/MainActivity.java
+++ app/src/main/java/com/example/sbook/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.sbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.widget.NestedScrollView;
 
 import android.content.Intent;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
     Boolean set_big_text = false;
     Boolean set_hints = true;
     int set_color = 0xE6E100;
-    int set_opacity = 0x88;
+    int set_opacity = 50;
+    //Элементы активити
     private LinearLayout mySLayout;
     private NestedScrollView mySView;
 
@@ -42,6 +47,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putStringArrayList("notes",myNotes);                                               //Запоминаем все заметки
+        outState.putBoolean("set_big_text",set_big_text);
+        outState.putBoolean("set_hints",set_hints);
+        outState.putInt("set_color",set_color);
+        outState.putInt("set_opacity",set_opacity);
     }
     //Генерация блока с заметкой
     protected void createTViews (int id) {
@@ -55,12 +64,17 @@ public class MainActivity extends AppCompatActivity {
         noteView.setLayoutParams(params);                                                           //применяем отступы
         noteView.setId(id);                                                                         //устанавливаем id
         noteView.setText(myNotes.get(id - 1));                                                      //добавляем в блок текст заметки
-        noteView.setTextSize(20);                                                                   //устанавалием размер шрифта
+        if (set_big_text) {noteView.setTextSize(20);}
+        else {noteView.setTextSize(20);}                                                                   //устанавалием размер шрифта
         noteView.setMinHeight(140);                                                                 //устанавалием минимальную высоту блока
         noteView.setMaxHeight(300);                                                                 //устанавливаемм максимальную высоту блока
         noteView.setTextColor(0xFF000000);                                                          //устанаваливаем цвет текста заметки
         noteView.setGravity(Gravity.CENTER_VERTICAL);                                               //выравниваем текст заметки по центру вертикально
-        noteView.setBackgroundResource(R.drawable.rectangle_2);                                     //придаем блоку форму
+        //noteView.setBackgroundResource(R.drawable.rectangle_2);                                     //придаем блоку форму
+        Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.rectangle_2);
+        ColorFilter colorFilter = new PorterDuffColorFilter(0x1000000 * (set_opacity + 100)  + set_color, PorterDuff.Mode.SRC_IN);
+        drawable.setColorFilter(colorFilter);
+        noteView.setBackground(drawable);
         noteView.setOnClickListener(new View.OnClickListener() {                                    //добавляем обработчик нажатия
             @Override
             public void onClick(View view) {
@@ -80,6 +94,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        set_big_text = savedInstanceState.getBoolean("set_big_text");
+        set_hints = savedInstanceState.getBoolean("set_hints");
+        set_color = savedInstanceState.getInt("set_color");
+        set_opacity = savedInstanceState.getInt("set_opacity");
         myNotes = savedInstanceState.getStringArrayList("notes");                               //восстанавливаем сохраненные ранее заметки
         for (int i = 1; i <= myNotes.size(); i++) {                                                 //пересоздаем scrollview с блоками заметок
             createTViews(i);
@@ -103,26 +121,38 @@ public class MainActivity extends AppCompatActivity {
         if (note == null) {                                                                         //если не вернулись данные завершаем процедуру
             return;
         }
-        int id = note.getIntExtra("id",-1);                                         //забираем из данных id заметки
-        if (id > -1 & id <= myNotes.size()) {                                                       //если заметка ранее существовала
-            myNotes.set(id-1, note.getStringExtra("result"));                                 //обновляем ее содержимое отредактированными данными
-            TextView tVNotes = findViewById(id);                                                    //находим блок этой заметки
-            tVNotes.setText(myNotes.get(id-1));                                                     //изменяем текст в блоке
-            if (myNotes.get(id-1).length() == 0) {                                                  //если наш текст пустой
-                myNotes.remove(id-1);                                                         //удаляем заметку
-                mySLayout.removeView(tVNotes);                                                      //удаляем блок
-                if (id <= myNotes.size()) {                                                         //если заметка не была последней в списке
-                    for (int i = id; i <= myNotes.size(); i++) {                                    //пробегаем по всем заметкам после нее
-                        tVNotes = findViewById(i+1);                                                //находим блоки
-                        tVNotes.setId(i);                                                           //уменьшаем их id на единицу
+        if (requestCode == 1) {
+            int id = note.getIntExtra("id", -1);                                    //забираем из данных id заметки
+            if (id > -1 & id <= myNotes.size()) {                                                   //если заметка ранее существовала
+                myNotes.set(id - 1, note.getStringExtra("result"));                           //обновляем ее содержимое отредактированными данными
+                TextView tVNotes = findViewById(id);                                                //находим блок этой заметки
+                tVNotes.setText(myNotes.get(id - 1));                                               //изменяем текст в блоке
+                if (myNotes.get(id - 1).length() == 0) {                                            //если наш текст пустой
+                    myNotes.remove(id - 1);                                                   //удаляем заметку
+                    mySLayout.removeView(tVNotes);                                                  //удаляем блок
+                    if (id <= myNotes.size()) {                                                     //если заметка не была последней в списке
+                        for (int i = id; i <= myNotes.size(); i++) {                                //пробегаем по всем заметкам после нее
+                            tVNotes = findViewById(i + 1);                                          //находим блоки
+                            tVNotes.setId(i);                                                       //уменьшаем их id на единицу
+                        }
                     }
                 }
+                return;
             }
-            return;
+            if (id > myNotes.size()) {                                                              //если у нас новая заметка
+                myNotes.add(note.getStringExtra("result"));                                   //добавляем ее в заметки
+                createTViews(id);                                                                   //создаем для нее блок
+            }
         }
-        if (id > myNotes.size()) {                                                                  //если у нас новая заметка
-            myNotes.add(note.getStringExtra("result"));                                       //добавляем ее в заметки
-            createTViews(id);                                                                       //создаем для нее блок
+        if (requestCode == 2) {
+
+            set_big_text = note.getBooleanExtra("set_big_text", false);
+            set_hints = note.getBooleanExtra("set_hints",true);
+            set_color = note.getIntExtra("set_color",0xE6E100);
+            set_opacity = note.getIntExtra("set_opacity",50);
+            Log.d("info", "bigtext: "+String.valueOf(set_big_text));
+            Log.d("info", "hints: "+String.valueOf(set_hints));
+            return;
         }
     }
     public void intro (View ImageButton) {
@@ -132,5 +162,14 @@ public class MainActivity extends AppCompatActivity {
         TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
         if( v != null) v.setGravity(Gravity.CENTER);
         toast.show();
+    }
+
+    public void settings (View Imagebutton) {
+        Intent sett = new Intent(this,  SettingsActivity.class);
+        sett.putExtra("set_big_text", set_big_text);
+        sett.putExtra("set_hints",set_hints);
+        sett.putExtra("set_color",set_color);
+        sett.putExtra("set_opacity",set_opacity);
+        startActivityForResult(sett, 2);
     }
 }
--- app/src/main/java/com/example/sbook/SettingsActivity.java
+++ app/src/main/java/com/example/sbook/SettingsActivity.java
@@ -1,14 +1,173 @@
 package com.example.sbook;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static java.lang.Math.log;
+import static java.lang.Math.round;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.slider.Slider;
 
 public class SettingsActivity extends AppCompatActivity {
+    Boolean set_big_text = false;
+    Boolean set_hints = true;
+    int set_color = 0xE6E100;
+    int set_opacity = 50;
 
+    TextView tVNotes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            set_big_text = bundle.getBoolean("set_big_text");
+            set_hints = bundle.getBoolean("set_hints");
+            set_color = bundle.getInt("set_color");
+            set_opacity = bundle.getInt("set_opacity");
+            SwitchCompat bigText = (SwitchCompat) findViewById(R.id.sw_big_text);
+            Slider colorSlider = findViewById(R.id.sw_color);
+            colorSlider.setValueFrom(0);
+            colorSlider.setValueTo(0xFFFFFF);
+            colorSlider.setStepSize(1);
+            colorSlider.setValue(set_color);
+            CheckBox hints = (CheckBox) findViewById(R.id.cb_hints);
+            SeekBar opacity = (SeekBar) findViewById(R.id.sb_opacity);
+            bigText.setChecked(set_big_text);
+            tVNotes = findViewById(R.id.tV_note);
+            bigText.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        set_big_text = true;
+                        tVNotes.setTextSize(30);
+                        if (set_hints) {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Сочувствую\nЯ тоже плохо вижу!", Toast.LENGTH_SHORT);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            TextView v = (TextView) toast.getView().findViewById(android.R.id.message);                 //формируем и выводим мотивирующий тост
+                            if (v != null) v.setGravity(Gravity.CENTER);
+                            toast.show();
+                        }
+                    } else {
+                        set_big_text = false;
+                        tVNotes.setTextSize(20);
+                        if (set_hints) {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Очень рада\nза твоё зрение!", Toast.LENGTH_SHORT);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            TextView v = (TextView) toast.getView().findViewById(android.R.id.message);                 //формируем и выводим мотивирующий тост
+                            if (v != null) v.setGravity(Gravity.CENTER);
+                            toast.show();
+                        }
+                    }
+                }
+            });
+            hints.setChecked(set_hints);
+            hints.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        set_hints = true;
+                        Toast toast = Toast.makeText(getApplicationContext(), "Люблю подсказывать!", Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        TextView v = (TextView) toast.getView().findViewById(android.R.id.message);                 //формируем и выводим мотивирующий тост
+                        if( v != null) v.setGravity(Gravity.CENTER);
+                        toast.show();
+                    } else {
+                        set_hints = false;
+                        Toast toast = Toast.makeText(getApplicationContext(), "Хозяин - барин!", Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        TextView v = (TextView) toast.getView().findViewById(android.R.id.message);                 //формируем и выводим мотивирующий тост
+                        if( v != null) v.setGravity(Gravity.CENTER);
+                        toast.show();
+                    }
+                }
+            });
+            opacity.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    set_opacity = seekBar.getProgress();
+                    changeBoxColor();
+                    // Обработчик изменений в SeekBar
+                }
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                    // Обработчик начала перемещения ползунка в SeekBar
+                }
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    if (set_hints) {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Ок!", Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        TextView v = (TextView) toast.getView().findViewById(android.R.id.message);                 //формируем и выводим мотивирующий тост
+                        if (v != null) v.setGravity(Gravity.CENTER);
+                        toast.show();
+                    }
+                }
+
+            });
+            opacity.setProgress(set_opacity);
+            colorSlider.addOnChangeListener(new Slider.OnChangeListener() {
+                @Override
+                public void onValueChange(Slider slider, float value, boolean fromUser) {
+                    set_color = round(value);
+                    changeBoxColor();
+                }
+            });
+        }
+    }
+    public void changeBoxColor () {
+        Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.rectangle_2);
+        ColorFilter colorFilter = new PorterDuffColorFilter(0x1000000 * (set_opacity + 100)  + set_color, PorterDuff.Mode.SRC_IN);
+        drawable.setColorFilter(colorFilter);
+        tVNotes.setBackground(drawable);
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean("set_big_text",set_big_text);
+        outState.putBoolean("set_hints",set_hints);
+        outState.putInt("set_color",set_color);
+        outState.putInt("set_opacity",set_opacity);
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        set_big_text = savedInstanceState.getBoolean("set_big_text");
+        set_hints = savedInstanceState.getBoolean("set_hints");
+        set_color = savedInstanceState.getInt("set_color");
+        set_opacity = savedInstanceState.getInt("set_opacity");
+    }
+    public void accept (View ImageButton) {
+        Intent sett = new Intent();
+        sett.putExtra("set_big_text", set_big_text);
+        sett.putExtra("set_hints", set_hints);
+        sett.putExtra("set_color",set_color);
+        sett.putExtra("set_opacity",set_opacity);
+        setResult(RESULT_OK,sett);
+        finish();
+    }
+    public void cancel (View ImageButton) {
+        Intent sett = new Intent();
+        setResult(RESULT_OK,sett);
+        finish();
     }
 }
